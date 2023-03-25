4055d47b9810881bf1b565f8a9d070a931b8f7b7
rwanoux <erwan_lemaire@yahoo.fr>
2023-03-25 15:11:53 +0100

    first commit

---
+++ scripts/module.js
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2020 Cody Swendrowski
+//
+// SPDX-License-Identifier: MIT
+
+import DollInventorySheet from "./sheet/DollInventorySheet.mjs";
+
+Hooks.once('init', async function () {
+    Actors.registerSheet("dnd5e", DollInventorySheet, {
+        types: ["character"],
+        makeDefault: true,
+    });
+});
+
+Hooks.once('ready', async function () {
+
+});
---
+++ scripts/sheet/DollInventorySheet.mjs
@@ -0,0 +1,5 @@
+import Tidy5eSheet from '../../../tidy5e-sheet/scripts/tidy5e-sheet.js'
+
+export default class DollInventorySheet extends Tidy5eSheet {
+
+}
